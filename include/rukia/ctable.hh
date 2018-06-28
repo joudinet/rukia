0e740780c1e56b27ed6d356cd0d9c60c416095dc
Johan Oudinet <johan.oudinet@green-communications.fr>
2018-06-28 17:23:46 +0200

    Fix a temporary bound warning
    The vertex index property map of a boost CSR graph is stored inside
    the counting_table object to be accessed whenever this object needs
    to map a vertex to an index.
    As described in the property map documentation:
    "Copying a property map should be inexpensive since they are often
    passed by value."
    So simply keep a real copy instead of a reference.

--- include/rukia/ctable.hh
+++ include/rukia/ctable.hh
@@ -1,20 +1,20 @@
-// Copyright (C) Univ. Paris-SUD, Johan Oudinet <oudinet@lri.fr> - 2007, 2008, 2009, 2010
-//  
+// Copyright (C) Univ. Paris-SUD, Johan Oudinet <oudinet@lri.fr> - 2007, 2018
+//
 // This file is part of Rukia.
-//  
+//
 // Rukia is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-//  
+//
 // Rukia is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//  
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with Rukia.  If not, see <http://www.gnu.org/licenses/>.
-//  
+//
 #ifndef RUKIA_CTABLE_HH
 # define RUKIA_CTABLE_HH
 # include <iostream>
@@ -24,29 +24,33 @@
 # include <boost/graph/iteration_macros.hpp>
 
 namespace rukia {
-  template<class Automaton, class T = double,
-	   class IndexMap = typename boost::property_map<Automaton, boost::vertex_index_t>::const_type>
+  template<class Automaton,
+	   class T = double,
+	   class VertexIndexMap =
+	   typename boost::property_map<Automaton,
+					boost::vertex_index_t>::const_type>
   class counting_table
   {
   public:
-    typedef typename boost::graph_traits<Automaton>::vertex_descriptor	vertex_descriptor;
-    typedef T	value_type;
-  
-    // Here I suppose all states are final, so I set ctable_ values to 1.
+    using vertex_descriptor	=
+      typename boost::graph_traits<Automaton>::vertex_descriptor;
+    using value_type		= T;
+    using vertex_index_map_type = VertexIndexMap;
+
+    // Assume all final states, so initialize ctable_ values to 1.
     counting_table (const Automaton& aut, unsigned n)
       : ctable_ (num_vertices(aut)*(n+1), 1), n_(n),
-	i_map(get(boost::vertex_index, aut))
+	vi_map(get(boost::vertex_index, aut))
     {
       compute_ctable_values (aut);
     }
-    
+
     // fname file lists final states.
     counting_table (const Automaton& aut, unsigned n, const char* fname)
       : ctable_ (num_vertices(aut)*(n+1), 0), n_(n),
-	i_map(get (boost::vertex_index, aut))
+	vi_map(get (boost::vertex_index, aut))
     {
-      typedef std::set<unsigned>	ids_type;
-      ids_type ids;
+      std::set<unsigned> ids;
       std::ifstream in (fname);
       while (in.good ())
 	{
@@ -56,10 +60,10 @@ namespace rukia {
 	}
       BGL_FORALL_VERTICES_T(s, aut, Automaton)
 	{
-	  ids_type::iterator it = ids.find (get(i_map, s));
+	  auto it = ids.find (get(vi_map, s));
 	  if (it != ids.end ())
 	    {
-	      std::cerr << get(i_map, s) << " is final." << std::endl;
+	      std::cerr << get(vi_map, s) << " is final." << std::endl;
 	      (*this)(s, 0) = 1;
 	      ids.erase (it);
 	      if (ids.empty ())
@@ -73,23 +77,23 @@ namespace rukia {
     template <class V>
     counting_table (const Automaton& aut, unsigned n, const std::vector<V>& L0)
       : ctable_ (num_vertices(aut)*(n+1)), n_(n),
-	i_map(get (boost::vertex_index, aut))
+	vi_map(get (boost::vertex_index, aut))
     {
       BGL_FORALL_VERTICES_T(s, aut, Automaton)
-	(*this)(s, 0) = L0[get(i_map, s)];
+	(*this)(s, 0) = L0[get(vi_map, s)];
       compute_ctable_values (aut);
     }
 
     T& operator()(vertex_descriptor s, unsigned n)
     {
       assert (n <= n_);
-      return ctable_[get(i_map, s) * (n_+1) + n];
+      return ctable_[get(vi_map, s) * (n_+1) + n];
     }
-    
+
     T operator()(vertex_descriptor s, unsigned n) const
     {
       assert (n <= n_);
-      return ctable_[get(i_map, s) * (n_+1) + n];
+      return ctable_[get(vi_map, s) * (n_+1) + n];
     }
 
     unsigned size() const { return n_; }
@@ -114,8 +118,8 @@ namespace rukia {
 
     std::vector<T> ctable_;
     unsigned n_;
-//     const Automaton& aut_;
-    const IndexMap& i_map;
+    /// A map to get an index from a vertex.
+    VertexIndexMap vi_map;
   };
 } // end namespace RUKIA
 
--- include/rukia/dtable.hh
+++ include/rukia/dtable.hh
@@ -1,20 +1,20 @@
-// Copyright (C) Univ. Paris-SUD, Johan Oudinet <oudinet@lri.fr> - 2010
-//  
+// Copyright (C) Univ. Paris-SUD, Johan Oudinet <oudinet@lri.fr> - 2010, 2018
+//
 // This file is part of Rukia.
-//  
+//
 // Rukia is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-//  
+//
 // Rukia is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//  
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with Rukia.  If not, see <http://www.gnu.org/licenses/>.
-//  
+//
 #ifndef RUKIA_DTABLE_HH
 # define RUKIA_DTABLE_HH
 # include <iostream>
@@ -24,32 +24,38 @@
 # include <boost/graph/iteration_macros.hpp>
 
 namespace rukia {
-    static const int MultiplyDeBruijnBitPosition2[32] = 
+    static const int MultiplyDeBruijnBitPosition2[32] =
       {
-	0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8, 
+	0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
 	31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
       };
 
-  template<class Automaton, class T = double,
-	   class IndexMap = typename boost::property_map<Automaton, boost::vertex_index_t>::const_type>
+  template<class Automaton,
+	   class T = double,
+	   class VertexIndexMap =
+	   typename boost::property_map<Automaton,
+					boost::vertex_index_t>::const_type>
   class dac_table
   {
   public:
-    typedef typename boost::graph_traits<Automaton>::vertex_descriptor	vertex_descriptor;
-    typedef typename boost::graph_traits<Automaton>::vertices_size_type	vertices_size_type;
-    typedef T	value_type;
-  
+    using vertex_descriptor	=
+      typename boost::graph_traits<Automaton>::vertex_descriptor;
+    using vertices_size_type	=
+      typename boost::graph_traits<Automaton>::vertices_size_type;
+    using value_type		= T;
+    using vertex_index_map_type	= VertexIndexMap;
+
     dac_table (const Automaton& aut, unsigned n)
       : k_(lg(n)),
 	dtable_(num_vertices(aut)*num_vertices(aut)*(k_+1), 0),
-	i_map(get(boost::vertex_index, aut)),
+	vi_map(get(boost::vertex_index, aut)),
 	q_(num_vertices(aut))
     {
       BGL_FORALL_EDGES_T(e, aut, Automaton)
 	{
 	  (*this)(source(e,aut), target(e,aut), 0) += 1;
 	}
-      
+
       for (unsigned i = 1; i <= k_; ++i)
 	{
 	  BGL_FORALL_VERTICES_T(s, aut, Automaton)
@@ -69,10 +75,8 @@ namespace rukia {
     T operator()(vertex_descriptor s, vertex_descriptor t, unsigned k) const
     {
       assert (k <= k_);
-      return dtable_[get(i_map, s) * q_ * (k_+1) + get(i_map, t) * (k_+1) + k];
+      return dtable_[get(vi_map, s) * q_ * (k_+1) + get(vi_map, t) * (k_+1) + k];
     }
-
-    // unsigned size() const { return k_; }
 
     // Fast computation of lg(n)
     static unsigned lg(unsigned n)
@@ -85,17 +89,17 @@ namespace rukia {
     T& operator()(vertex_descriptor s, vertex_descriptor t, unsigned k)
     {
       assert (k <= k_);
-      return dtable_[get(i_map, s) * q_ * (k_+1) + get(i_map, t) * (k_+1) + k];
+      return dtable_[get(vi_map, s) * q_ * (k_+1) + get(vi_map, t) * (k_+1) + k];
     }
-    
+
     // copy and assignment are not allowed
     dac_table (const dac_table&);
     dac_table& operator= (const dac_table&);
 
-    unsigned k_;
-    std::vector<T> dtable_;
-    const IndexMap& i_map;
-    vertices_size_type q_;
+    unsigned			k_;
+    std::vector<T>		dtable_;
+    vertex_index_map_type	vi_map;
+    vertices_size_type		q_;
   };
 } // end namespace RUKIA
 
